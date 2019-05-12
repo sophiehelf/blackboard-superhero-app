aecfda78a1122195ee68d043f8888fb6970c1ed9
Sophie <sophie.helf@gmail.com>
2019-05-12 13:30:54 -0400

    layering components more

---
+++ src/CharacterContainer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function CharacterContainer(props) {
+    return <p>is this working</p>
+}
+
+export default CharacterContainer
--- src/Container.js
+++ src/Container.js
@@ -1,27 +1,8 @@
 import React from 'react';
+import CharacterContainer from './CharacterContainer.js'
 
-class Container extends React.Component {
-    constructor(props) {
-        super(props) 
-        this.state = {
-            list: [
-                "blah",
-                "who cares",
-                "something",
-                "something else"
-            ]
-        }
-    }
-
-
-    render() {
-        return (
-            <div className="container">
-                <p>hi</p>
-            </div>
-
-        )
-    }
+function Container(props) {
+    return <CharacterContainer />
 }
 
 export default Container 
